5b2504d0e351814baaecfc40639562f7f6f431ef
Reed Essick <reed@rbe4.org>
2015-08-15 22:50:20 -0400

    modified tests to be a bit cleaner. Also added the beginnings of a test for binomial_bs

--- test.py
+++ test.py
@@ -2,13 +2,17 @@ import time
 
 from greedyCI import *
 
-n = 20
+### poisson test
+n = 4
 conf = 0.68
 
-y = []
-for x in xrange(100):
-    to = time.time()
-    klow, khigh = poisson_bs(conf, n)
-    y.append( time.time()-to )
+klow, khigh = poisson_bs(conf, n)
+assert((abs(klow-2.2976226806640625)<1e-4) and (abs(khigh-6.3874831975541984)<1e-4))
 
-print np.mean(y), np.std(y)
+### binomial test
+n = 4
+N = 20
+conf = 0.68
+
+klow, khigh = binomial_bs(conf, n, N)
+
